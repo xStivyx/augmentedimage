543a789b0ee97a3fcfcc38d514ad9ed600ff0cd4
Stephen <scha9393@gmail.com>
2019-08-26 09:43:32 +0900

    first

---
+++ app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageActivity.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ar.sceneform.samples.augmentedimage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Pose;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.samples.common.helpers.SnackbarHelper;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This application demonstrates using augmented images to place anchor nodes. app to include image
+ * tracking functionality.
+ *
+ * <p>In this example, we assume all images are static or moving slowly with a large occupation of
+ * the screen. If the target is actively moving, we recommend to check
+ * ArAugmentedImage_getTrackingMethod() and render only when the tracking method equals to
+ * AR_AUGMENTED_IMAGE_TRACKING_METHOD_FULL_TRACKING. See details in <a
+ * href="https://developers.google.com/ar/develop/c/augmented-images/">Recognize and Augment
+ * Images</a>.
+ */
+public class AugmentedImageActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private ModelRenderable andyRenderable;
+    private ImageView fitToScanView;
+    private HitResult hitResult;
+
+    // Augmented image and its associated center pose anchor, keyed by the augmented image in
+    // the database.
+    private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.v("케챱!!!", "인식완료");
+
+            }
+        });
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        fitToScanView = findViewById(R.id.image_view_fit_to_scan);
+
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+
+        // When you build a Renderable, Sceneform loads its resources in the background while returning
+        // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+        ModelRenderable.builder()
+                .setSource(this, R.raw.ketchup)
+                .build()
+                .thenAccept(renderable -> andyRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (andyRenderable == null) {
+                        return;
+                    }
+
+
+                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (augmentedImageMap.isEmpty()) {
+            fitToScanView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Registered with the Sceneform Scene object, this method is called at the start of each frame.
+     *
+     * @param frameTime - time since last frame.
+     */
+    private void onUpdateFrame(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        // If there is no frame, just return.
+        if (frame == null) {
+            return;
+        }
+
+        Collection<AugmentedImage> updatedAugmentedImages =
+                frame.getUpdatedTrackables(AugmentedImage.class);
+        for (AugmentedImage augmentedImage : updatedAugmentedImages) {
+            switch (augmentedImage.getTrackingState()) {
+                case PAUSED:
+                    // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+                    // but not yet tracked.
+                    String text = "Detected Image " + augmentedImage.getIndex();
+                    SnackbarHelper.getInstance().showMessage(this, text);
+                    break;
+
+                case TRACKING:
+                    // Have to switch to UI Thread to update View.
+                    fitToScanView.setVisibility(View.GONE);
+
+                    // Create a new anchor for newly found images.
+                    if (!augmentedImageMap.containsKey(augmentedImage)) {
+
+                        switch (augmentedImage.getIndex()) {
+                            case 0: {
+
+                            }
+                            break;
+
+                            case 1:{
+
+                            }
+                            break;
+                        }
+
+                        Anchor anchor = augmentedImage.createAnchor(augmentedImage.getCenterPose());
+                        AnchorNode aNode = new AnchorNode(anchor);
+                        aNode.setParent(arFragment.getArSceneView().getScene());
+
+                        AugmentedImageNode augNode = new AugmentedImageNode(this);
+                        //augNode.setImage(augmentedImage);
+                        augmentedImageMap.put(augmentedImage, augNode);
+                        //arFragment.getArSceneView().getScene().addChild(augNode);
+                        //augNode.setLocalScale(new Vector3(0.1f, 0.1f, 0.1f));
+
+                        TransformableNode tNode = new TransformableNode(arFragment.getTransformationSystem());
+                        tNode.setParent(aNode);
+
+                        tNode.setRenderable(andyRenderable);
+                        //tNode.setLocalScale(new Vector3(1f, 1f, 1f));
+                        tNode.select();
+                    }
+                    break;
+
+                case STOPPED:
+                    augmentedImageMap.remove(augmentedImage);
+                    break;
+            }
+        }
+    }
+}
